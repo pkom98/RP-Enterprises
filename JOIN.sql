8670b7b0b8d33effb3c183c691ea6a2c788765a2
Pawel <you@example.com>
2022-11-15 14:35:56 +0100

    test1

--- JOIN.sql
+++ JOIN.sql
@@ -1,3 +1,3 @@
-SELECT produkty.model, producenci.nazwa, elo, test_trzeciego_brancha
+SELECT produkty.model, producenci.nazwa, elo, test do mastera
 FROM produkty INNER JOIN producenci
 ON produkty.producent = producenci.id_producenta;
