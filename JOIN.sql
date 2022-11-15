190fc532d664c522170a5acd57eedc391e27a166
Pawel <you@example.com>
2022-11-15 15:07:36 +0100

    nowy test do mastera 4b

--- JOIN.sql
+++ JOIN.sql
@@ -1,3 +1,3 @@
-SELECT produkty.model, producenci.nazwa, elo, test do mastera22222
+SELECT produkty.model, producenci.nazwa, elo, test do mastera22222 4branch
 FROM produkty INNER JOIN producenci
 ON produkty.producent = producenci.id_producenta;
