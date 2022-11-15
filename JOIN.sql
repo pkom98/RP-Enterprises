6aa39dbe73641bda6505c56f0cf4346c97468fa7
Pawel <you@example.com>
2022-11-15 09:08:45 +0100

    fourth commit testing 3rd branch

--- JOIN.sql
+++ JOIN.sql
@@ -1,3 +1,3 @@
-SELECT produkty.model, producenci.nazwa, elo
+SELECT produkty.model, producenci.nazwa, elo, test_trzeciego_brancha
 FROM produkty INNER JOIN producenci
 ON produkty.producent = producenci.id_producenta;
